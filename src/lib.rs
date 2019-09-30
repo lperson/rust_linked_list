dd71594d76baa009f81d836569d2c247b472c5d6
Larry Person <lp10011@gmail.com>
2019-09-30 19:39:17 -0400

    Added prev to node and tail to the list, which enable us to implement peek_back and DoubleEndedIterator.

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 use std::boxed::Box;
-use std::iter::{Iterator, IntoIterator};
+use std::iter::{IntoIterator, Iterator};
+use std::ptr::null_mut;
 
 // typr alias
 type Link<T> = Option<Box<Node<T>>>;
@@ -7,22 +8,28 @@ type Link<T> = Option<Box<Node<T>>>;
 pub struct Node<T> {
     data: T,
     next: Link<T>,
+    prev: *mut Node<T>,
 }
 
 impl<T> Node<T> {
-    fn new(data: T, next: Link<T>) -> Self {
-        Self { data, next }
+    fn new(data: T, next: Link<T>, prev: *mut Node<T>) -> Self {
+        Self { data, next, prev }
     }
 }
 
 pub struct SimpleLinkedList<T> {
     head: Link<T>,
     len: usize,
+    tail: *mut Node<T>,
 }
 
 impl<'a, T> Default for SimpleLinkedList<T> {
     fn default() -> Self {
-        Self { head: None, len: 0 }
+        Self {
+            head: None,
+            len: 0,
+            tail: null_mut(),
+        }
     }
 }
 
@@ -88,9 +95,13 @@ impl<T> SimpleLinkedList<T> {
         self.head.is_none()
     }
 
-    pub fn iter(&self) -> NodeIter<T> {
+    pub fn iter(&self) -> NodeIter<T>
+    where
+        T: std::fmt::Debug,
+    {
         NodeIter {
             next: self.head.as_ref().map::<&Node<T>, _>(|node| node),
+            next_back: self.tail,
         }
     }
 
@@ -105,7 +116,26 @@ impl<T> SimpleLinkedList<T> {
     }
 
     pub fn push(&mut self, jawn: T) {
-        let new_jawn = Box::new(Node::new(jawn, self.head.take()));
+        let mut old_head = self.head.take();
+        let mut old_head_ptr: *mut Node<T> = null_mut();
+
+        if let Some(some_old_head) = old_head.as_mut() {
+            old_head_ptr = &mut **some_old_head;
+        }
+
+        let mut new_jawn = Box::new(Node::new(jawn, old_head, null_mut()));
+        let new_jawn_ptr: *mut _ = &mut *new_jawn;
+
+        if !old_head_ptr.is_null() {
+            unsafe {
+                (*old_head_ptr).prev = new_jawn_ptr;
+            }
+        }
+
+        if self.tail.is_null() {
+            self.tail = new_jawn_ptr;
+        }
+
         self.head = Some(new_jawn);
         self.len += 1;
     }
@@ -114,6 +144,11 @@ impl<T> SimpleLinkedList<T> {
         if let Some(old_head) = self.head.take() {
             self.head = old_head.next;
             self.len -= 1;
+            if let Some(head) = self.head.as_mut() {
+                head.prev = null_mut();
+            } else {
+                self.tail = null_mut();
+            }
             return Some(old_head.data);
         }
         None
@@ -122,6 +157,16 @@ impl<T> SimpleLinkedList<T> {
     pub fn peek(&self) -> Option<&T> {
         if let Some(head) = self.head.as_ref() {
             return Some(&head.data);
+        }
+        None
+    }
+
+    pub fn peek_back(&self) -> Option<&T> {
+        if !self.tail.is_null() {
+            unsafe {
+                let tail = &*self.tail;
+                return Some(&tail.data);
+            }
         }
         None
     }
@@ -140,6 +185,7 @@ impl<T> SimpleLinkedList<T> {
 
 pub struct NodeIter<'a, T> {
     next: Option<&'a Node<T>>,
+    next_back: *const Node<T>,
 }
 
 pub struct MutNodeIter<'a, T> {
@@ -154,6 +200,22 @@ impl<'a, T: 'a> Iterator for NodeIter<'a, T> {
             self.next = node.next.as_ref().map(|node| &**node);
             &node.data
         })
+    }
+}
+
+impl<'a, T: 'a> DoubleEndedIterator for NodeIter<'a, T>
+where
+    T: std::fmt::Debug,
+{
+    fn next_back(&mut self) -> Option<Self::Item> {
+        if !self.next_back.is_null() {
+            let next_back = self.next_back;
+            unsafe {
+                self.next_back = (*next_back).prev;
+                return Some(&(*next_back).data);
+            }
+        }
+        None
     }
 }
 
@@ -218,6 +280,44 @@ mod test {
         let value = iterated_list.next().unwrap();
         *value = 4u32;
 
-        assert_eq!(list.peek(), Some(&4) );
+        assert_eq!(list.peek(), Some(&4));
+    }
+
+    #[test]
+    fn test_peek_back() {
+        let mut list: SimpleLinkedList<u32> = SimpleLinkedList::new();
+        assert_eq!(
+            list.peek_back(),
+            None,
+            "No element should be contained in list"
+        );
+        list.push(2);
+        assert_eq!(list.peek_back(), Some(&2), "Element must be 2");
+        assert_eq!(list.peek_back(), Some(&2), "Element must be still 2");
+        list.push(3);
+        assert_eq!(list.peek_back(), Some(&2), "Head is still 3");
+        assert_eq!(list.pop(), Some(3), "Element must still be 3");
+        assert_eq!(list.peek_back(), Some(&2), "Head element is now 2");
+        assert_eq!(list.pop(), Some(2), "Element must be 2");
+        assert_eq!(
+            list.peek_back(),
+            None,
+            "No element should be contained in list"
+        );
+    }
+
+    #[test]
+    fn test_iter_rev() {
+        let mut list: SimpleLinkedList<u32> = SimpleLinkedList::new();
+        list.push(1);
+        list.push(2);
+        list.push(3);
+        list.push(4);
+        list.pop();
+        let mut rev_iter = list.iter().rev();
+        assert_eq!(rev_iter.next(), Some(&1));
+        assert_eq!(rev_iter.next(), Some(&2));
+        assert_eq!(rev_iter.next(), Some(&3));
+        assert_eq!(rev_iter.next(), None);
     }
 }
